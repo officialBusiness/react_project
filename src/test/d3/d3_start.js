97df68e6d40d1023a4bf0c4b0ac8fd5136c31f56
official business <wx_9e4c73a607f54399aa3916dbd1109e9f@git.code.tencent.com>
2021-06-24 20:19:43 +0800

    开始完善案例页面

--- src/test/d3/d3_start.js
+++ src/test/d3/d3_start.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import * as examples from './examples'
+import './d3_start.less'
 
 export default class D3Start extends React.Component{
 	componentDidMount(){
-
-		examples.circlePacking( this.container )
+		// examples.circlePacking( this.container )
 
 	}
 	render(){
 		return (
-			<div
-				ref={(dom)=>{
-					this.container = dom
-				}} 
+			<div id={'d3Start'}
 				style={{
 					width: '100%',
 					height: '100%'
 				}}>
-
+				<div className={'d3-list'}>列表数据</div>
+				<div ref={(dom)=>{
+						this.container = dom
+					}}></div>
 			</div>
 		)
 	}

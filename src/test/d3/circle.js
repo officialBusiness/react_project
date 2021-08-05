8f763cf83bf7af9bd53bd79cdb8fc933130dac35
official business <wx_9e4c73a607f54399aa3916dbd1109e9f@git.code.tencent.com>
2021-08-05 10:08:17 +0800

    备份

--- src/test/d3/circle.js
+++ src/test/d3/circle.js
@@ -8,13 +8,16 @@ export default function circle( container ){
 		.attr("height", '100%');
 
 	var circle = svg.selectAll("circle")
-		.data([32, 57, 293], function(d) { return d; });
+		.data([32, 57, 293]);
 
-	// console.log('circle:', circle)
+	console.log('circle:', circle)
+	console.log('circle.enter():', circle.enter())
 
 	circle.enter().append("circle")
 		.attr("cy", 60)
 		.attr("cx", function(d, i) { return i * 100 + 30; })
-		.attr("r", function(d) { return Math.sqrt(d); });
-
+		.attr("r", function(d) { return Math.sqrt(d); })
+		.attr("class", 't1 t2')
+		.classed('t1', true)
+		.classed('t2', true);
 }
